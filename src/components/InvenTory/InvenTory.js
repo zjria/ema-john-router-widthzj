babdf7fad1ed0a8c8568ce4d0fe6b27d72dd97f6
zjria <zakaria.com.bd@gmail.com>
2021-10-02 12:25:23 +0600

    first commit

---
+++ src/components/InvenTory/InvenTory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvenTory = () => {
+    return (
+        <div>
+            <h2>Hi Test now folder</h2>
+            
+        </div>
+    );
+};
+
+export default InvenTory;
